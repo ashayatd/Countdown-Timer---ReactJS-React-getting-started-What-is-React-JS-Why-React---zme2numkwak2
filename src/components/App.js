39da04fe355ea855a975f371a22ed802c6fc17ce
Ashay Tamrakar <48744162+ashayatd@users.noreply.github.com>
2022-11-04 11:07:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,41 @@
-import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
-
+import React, { Component, useState, useEffect } from “react”;
+import ‘../styles/App.css’;
 const App = () => {
-  // write your code here 
-
+  const [getTime,setTime]=useState(0);
+  let result=‘’;
+  // write your code here
+  useEffect(()=>{
+    if(getTime!=0){
+      result = setTimeout(()=>{
+        setTime(getTime-1);
+      },1000)
+    }
+  },[getTime])
+  const onKeyboardEnter=(event)=>{
+      if(event.keyCode === 13){
+        let input = document.getElementById(“timeCount”).value;
+        console.log(input!=0);
+        if(!isNaN(input) && input!=0){
+          console.log(input);
+          setTime(parseInt(input));
+        }
+        else{
+          setTime(0);
+          if(result!=“”)[
+            clearTimeout(result)
+          ]
+        }
+      }
+  }
   return (
-    <div className="wrapper">
-      <div id="whole-center">
+    <div className=“wrapper”>
+      <div id=“whole-center”>
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id=“timeCount” onKeyDown={onKeyboardEnter} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id=“current-time”>{getTime}</div>
     </div>
   )
 }
-
-
 export default App;
